7fe1b00812d5af7257057bae524adf281998af39
Patrick Kelley <pkelley@bombora.com>
2021-11-10 14:41:56 -0800

    Declare emacs bankruptcy: add chemacs

---
+++ .config/chemacs/profiles.el
@@ -0,0 +1,4 @@
+(("spacemacs" . ((user-emacs-directory . "/Users/pakelley/dotfiles/config/efreakingmacs/pacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
+ ("doom" . ((user-emacs-directory . "~/.config/doom-emacs")
+            (env . (("DOOMDIR" . "~/.config/doom"))))))
