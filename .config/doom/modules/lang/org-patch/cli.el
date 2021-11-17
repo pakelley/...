34cb09c8ee514e0bded1a67f7173badb650ff173
Patrick Kelley <pakelley@pm.me>
2021-11-17 15:46:58 -0800

    [doom] Setup for literate personal org module

---
+++ .config/doom/modules/lang/org-patch/cli.el
@@ -0,0 +1,7 @@
+;;; lang/org-patch/cli.el -*- lexical-binding: t; -*-
+
+(load! "autoload")
+
+;; Tangle the user's config.org before 'doom sync' runs
+(when (featurep! :config literate)
+  (add-hook 'doom-sync-pre-hook (apply-partially '+literate-tangle-module-h "lang" "org-patch")))
