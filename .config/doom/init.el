91284d8eaeb7f61428c6377f02d613fcd281562a
Patrick Kelley <pakelley@pm.me>
2022-03-25 21:30:06 -0700

    [doom] start of docker/k8s setup

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -88,11 +88,12 @@
        biblio
        (debugger +lsp)     ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
+       kubernetes-patch
        (lookup +docsets)   ; navigate your code and its documentation
        lsp                 ; M-x vscode
        (magit +forge)      ; a git porcelain for Emacs
---
+++ .config/doom/modules/tools/kubernetes-patch/cli.el
@@ -0,0 +1,7 @@
+;;; lang/python-patch/cli.el -*- lexical-binding: t; -*-
+
+(load! "autoload")
+
+;; Tangle the user's config.org before 'doom sync' runs
+(when (featurep! :config literate)
+  (add-hook 'doom-sync-pre-hook (apply-partially '+literate-patch/literate-tangle-module-h "tools" "kubernetes-patch")))
