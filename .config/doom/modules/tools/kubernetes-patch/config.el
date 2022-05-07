7706cbd8f3de014de8eaf0c027ea7c98ca97a08a
pakelley <pakelley@pm.me>
2022-05-06 17:11:39 -0700

    [doom] improve kubernetes

--- .config/doom/modules/tools/kubernetes-patch/config.el
+++ .config/doom/modules/tools/kubernetes-patch/config.el
@@ -1,1 +1,9 @@
-; TBD
+(use-package! kubernetes
+  :defer t
+  :commands (kubernetes-overview k8s)
+  :config
+  (fset 'k8s 'kubernetes-overview)
+)
+(use-package! kubernetes-evil
+  :defer t
+  :after kubernetes)
--- .config/doom/modules/tools/kubernetes-patch/packages.el
+++ .config/doom/modules/tools/kubernetes-patch/packages.el
@@ -1,2 +1,4 @@
 (package! kubernetes)
 (package! kubernetes-evil)
+;; (package! kubernetes-tramp)
+(package! k8s-mode)
