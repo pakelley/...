ca3854aeddf24871a08499a3a214307d92e37eac
Patrick Kelley <pkelley@twitter.com>
2022-06-17 17:37:54 -0700

    [doom] update org-agenda-files

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -1,9 +1,12 @@
+;;; ../.../.config/doom/custom.el -*- lexical-binding: t; -*-
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files '("~/Nextcloud/.local/share/notes/gtd/org-gtd-tasks.org"))
+ '(org-agenda-files '("~/.local/share/notes/gtd/org-gtd-tasks.org"))
  '(package-selected-packages
    '(blacken org-modern kubernetes-tramp kubernetes-evil org-super-agenda doct org-gtd))
  '(safe-local-variable-values
