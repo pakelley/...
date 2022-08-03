b47447a5399c023e7e8e1207f5025faec5360dc4
Patrick Kelley <pkelley@twitter.com>
2022-08-03 08:23:36 -0700

    [wez/nu] add wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require 'wezterm';
+
+return {
+  default_prog = { '/opt/homebrew/bin/nu' },
+
+  -- color_scheme = "Gruvbox Light",
+  color_scheme = "Japanesque",
+  -- color_scheme = "Tomorrow Night Burns",
+  -- color_scheme = "Zenburn",
+  -- color_scheme = "Subliminal",
+  -- color_scheme = "Seafoam Pastel",
+  -- color_scheme = "Paraiso Dark",
+  -- color_scheme = "Operator Mono Dark",
+  -- color_scheme = "Gruvbox Dark",
+  -- color_scheme = "Flatland",
+  -- color_scheme = "Ciapre",
+  -- color_scheme = "BirdsOfParadise",
+  -- color_scheme = "Belafonte Night",
+  -- color_scheme = "Belafonte Day",
+  -- color_scheme = "Novel",
+}
