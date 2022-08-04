ac5d6c5d1b6cd7805e2cf98683527a44c822d069
Patrick Kelley <pkelley@twitter.com>
2022-08-03 18:20:10 -0700

    [wez] add keybindings for panes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm';
+local act = wezterm.action
 
 return {
   default_prog = { '/opt/homebrew/bin/nu' },
@@ -20,4 +21,84 @@ return {
   -- color_scheme = "Belafonte Night",
   -- color_scheme = "Belafonte Day",
   -- color_scheme = "Novel",
+
+  leader = {key = "Space", mods = "CTRL"},
+  keys = {
+    {
+      mods = "LEADER",
+      key = "-",
+      action=act{SplitVertical={domain="CurrentPaneDomain"}}
+    },
+    {
+      mods = "LEADER",
+      key = "/",
+      action=act{SplitHorizontal={domain="CurrentPaneDomain"}}
+    },
+    {
+      key = 'r',
+      mods = 'LEADER',
+      action = act{ActivateKeyTable={
+        name = 'resize_pane',
+        one_shot = false,
+      }},
+    },
+
+    -- CTRL+SHIFT+Space, followed by 'w' will put us in activate-pane
+    -- mode until we press some other key or until 1 second (1000ms)
+    -- of time elapses
+    {
+      key = 'w',
+      mods = 'LEADER',
+      action = act{ActivateKeyTable={
+        name = 'activate_pane',
+        timeout_milliseconds = 1000,
+      }},
+    },
+    {
+      key = 'w',
+      mods = 'CMD',
+      action = act{CloseCurrentPane={ confirm = true }},
+    },
+  },
+  key_tables = {
+    -- Defines the keys that are active in our resize-pane mode.
+    -- Since we're likely to want to make multiple adjustments,
+    -- we made the activation one_shot=false. We therefore need
+    -- to define a key assignment for getting out of this mode.
+    -- 'resize_pane' here corresponds to the name="resize_pane" in
+    -- the key assignments above.
+    resize_pane = {
+      { key = 'LeftArrow', action = act{AdjustPaneSize={ 'Left', 1 }} },
+      { key = 'h', action = act{AdjustPaneSize={ 'Left', 1 }} },
+
+      { key = 'RightArrow', action = act{AdjustPaneSize={ 'Right', 1 }} },
+      { key = 'l', action = act{AdjustPaneSize={ 'Right', 1 }} },
+
+      { key = 'UpArrow', action = act{AdjustPaneSize={ 'Up', 1 }} },
+      { key = 'k', action = act{AdjustPaneSize={ 'Up', 1 }} },
+
+      { key = 'DownArrow', action = act{AdjustPaneSize={ 'Down', 1 }} },
+      { key = 'j', action = act{AdjustPaneSize={ 'Down', 1 }} },
+
+      -- Cancel the mode by pressing escape
+      { key = 'Escape', action = 'PopKeyTable' },
+    },
+
+    -- Defines the keys that are active in our activate-pane mode.
+    -- 'activate_pane' here corresponds to the name="activate_pane" in
+    -- the key assignments above.
+    activate_pane = {
+      { key = 'LeftArrow', action = act{ActivatePaneDirection='Left'} },
+      { key = 'h', action = act{ActivatePaneDirection='Left'} },
+
+      { key = 'RightArrow', action = act{ActivatePaneDirection='Right'} },
+      { key = 'l', action = act{ActivatePaneDirection='Right'} },
+
+      { key = 'UpArrow', action = act{ActivatePaneDirection='Up'} },
+      { key = 'k', action = act{ActivatePaneDirection='Up'} },
+
+      { key = 'DownArrow', action = act{ActivatePaneDirection='Down'} },
+      { key = 'j', action = act{ActivatePaneDirection='Down'} },
+    },
+  },
 }
