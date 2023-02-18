0eb3e236f6d6fb55465ad8f42db81860c996add4
Patrick Kelley <patrick@heartex.com>
2023-02-18 13:17:31 -0800

    [doom/python] add eglot/pdm connection

--- .config/doom/modules/lang/python-patch/config.el
+++ .config/doom/modules/lang/python-patch/config.el
@@ -11,3 +11,17 @@
   (numpydoc-insertion-style 'yas)
   :config
   (map! :localleader ("d" #'numpydoc-generate)))
+
+(defun +patch/get-pdm-packages-path ()
+  "For the current PDM project, find the path to the packages."
+  (let ((packages-path (string-trim (shell-command-to-string "pdm info --packages"))))
+    (concat packages-path "/lib")))
+
+(defun +patch/eglot-workspace-config (server)
+  "For the current PDM project, dynamically generate a python lsp config."
+  `(:python\.analysis (:extraPaths ,(vector (+patch/get-pdm-packages-path)))))
+
+(setq-default eglot-workspace-configuration #'+patch/eglot-workspace-config)
+
+;; while we're at it, remove doom's hook to stop hearing errors about it
+(remove-hook! 'python-mode-local-vars-hook #'+python-init-anaconda-mode-maybe-h)
