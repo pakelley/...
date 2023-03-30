d2241ed5299eb2253bfd6fa591d861059d4424e1
pakelley <patrick@heartex.com>
2023-03-29 20:21:30 -0700

    [notmuch] add email address

--- .config/goimapnotify/cron.sh
+++ .config/goimapnotify/cron.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
-
 set -e
-cd ~/.mail/account.kelleys-gmail
 
-date >> /tmp/gmailieer_patchwork_notmuch.log
-gmi sync >> /tmp/gmailieer_patchwork_notmuch.log 2>&1
+if [ -d "$HOME/.mail/account.kelleys-gmail" ]
+then
+    cd "$HOME/.mail/account.kelleys-gmail"
+    date >> /tmp/gmailieer_patchwork_notmuch.log
+    gmi sync >> /tmp/gmailieer_patchwork_notmuch.log 2>&1
+fi
+
+if [ -d "$HOME/.mail/account.heartex" ]
+then
+    cd "$HOME/.mail/account.heartex"
+    date >> /tmp/gmailieer_patchwork_notmuch.log
+    gmi sync >> /tmp/gmailieer_patchwork_notmuch.log 2>&1
+fi
 
 notmuch new
 # notmuch tag -new -- tag:new >> /tmp/gmailieer_patchwork_notmuch.log 2>&1
