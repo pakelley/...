0e6cabd10c6152038a8254245b4d2d265ef3cbe6
pakelley <patrick@heartex.com>
2024-01-10 11:04:43 -0800

    [doom/org] add habit config (and checkboxes)

--- .config/doom/modules/lang/org-patch/config.el
+++ .config/doom/modules/lang/org-patch/config.el
@@ -1998,3 +1998,34 @@
   (add-to-list 'org-agenda-files org-caldav-inbox)
   ;; sync every 1 hour
   (run-with-timer 0 (* 60 (* 60 1)) #'org-caldav-sync))
+
+(after! org
+  (setq org-todo-repeat-to-state "TODO"))
+
+(after! org
+  (defun +patch/org-checkbox-todo ()
+    "Switch header TODO state to DONE when all checkboxes are ticked, to TODO otherwise"
+    (let ((todo-state (org-get-todo-state)) beg end)
+      (unless (not todo-state)
+        (save-excursion
+          (org-back-to-heading t)
+          (setq beg (point))
+          (end-of-line)
+          (setq end (point))
+          (goto-char beg)
+          (if (re-search-forward "\\[\\([0-9]*%\\)\\]\\|\\[\\([0-9]*\\)/\\([0-9]*\\)\\]"
+                                 end t)
+              (if (match-end 1)
+                  (if (equal (match-string 1) "100%")
+                      (unless (string-equal todo-state "DONE")
+                        (org-todo 'done))
+                    (unless (string-equal todo-state "TODO")
+                      (org-todo 'todo)))
+                (if (and (> (match-end 2) (match-beginning 2))
+                         (equal (match-string 2) (match-string 3)))
+                    (unless (string-equal todo-state "DONE")
+                      (org-todo 'done))
+                  (unless (string-equal todo-state "TODO")
+                    (org-todo 'todo)))))))))
+
+  (add-hook 'org-checkbox-statistics-hook '+patch/org-checkbox-todo))
