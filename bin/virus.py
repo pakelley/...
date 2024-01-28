8bcc395ffdc1f05e048e2dfa9a6387cb2619bcad
pakelley <patrick@heartex.com>
2024-01-27 17:49:33 -0800

    [virus] format install file

--- bin/virus.py
+++ bin/virus.py
@@ -11,9 +11,9 @@ from simple_term_menu import TerminalMenu
 DOTFILE_PATH = pathlib.Path(__file__).parent.parent
 
 # logging setup
-RESET_SEQ = "\033[0m"
-COLOR_SEQ = "\033[1;%dm"
-BOLD_SEQ = "\033[1m"
+RESET_SEQ = '\033[0m'
+COLOR_SEQ = '\033[1;%dm'
+BOLD_SEQ = '\033[1m'
 BLUE = COLOR_SEQ % 34
 
 LOG = logging.getLogger(__name__)
@@ -22,48 +22,52 @@ _HANDLER = logging.StreamHandler()
 _HANDLER.setLevel(logging.DEBUG)
 # TODO base color on logging level
 _FORMATTER = logging.Formatter(
-    f"%(asctime)s - {BLUE}%(levelname)s{RESET_SEQ} - %(message)s"
+    f'%(asctime)s - {BLUE}%(levelname)s{RESET_SEQ} - %(message)s'
 )
 _HANDLER.setFormatter(_FORMATTER)
 LOG.addHandler(_HANDLER)
 
 # link config
 link_menu = TerminalMenu(
-    ["Yes", "No"],
-    title="Ok, now I need to link your config files to the right places. Cool with that?",
+    ['Yes', 'No'],
+    title='Ok, now I need to link your config files to the right places. Cool with that?',
 )
 may_i_please_link_configs = link_menu.show()
 if may_i_please_link_configs == 0:
-    LOG.info("Thank you, linking configs")
+    LOG.info('Thank you, linking configs')
     subprocess.run(
         [
-            "ln",
-            "-svf",
-            str(DOTFILE_PATH / ".mackup.cfg"),
-            str(pathlib.Path.home() / ".mackup.cfg"),
+            'ln',
+            '-svf',
+            str(DOTFILE_PATH / '.mackup.cfg'),
+            str(pathlib.Path.home() / '.mackup.cfg'),
         ]
     )
-    if (DOTFILE_PATH / ".mackup").exists():
+    if (DOTFILE_PATH / '.mackup').exists():
         subprocess.run(
             [
-                "ln",
-                "-svf",
-                str(DOTFILE_PATH / ".mackup"),
-                str(pathlib.Path.home() / ".mackup"),
+                'ln',
+                '-svf',
+                str(DOTFILE_PATH / '.mackup'),
+                str(pathlib.Path.home() / '.mackup'),
             ]
         )
-    subprocess.run(["mackup", "restore"])
+    subprocess.run(['mackup', 'restore'])
 else:
     LOG.info("Sorry to hear that, exiting as I cannot run without mackup :'(")
     sys.exit()
 
 # Install programs with brew
-brewpath = pathlib.Path.home() / ".config" / "homebrew"
+brewpath = pathlib.Path.home() / '.config' / 'homebrew'
 brew_specs = sorted(
     [
-        (brew_group.group("module"), path.name)
+        (brew_group.group('module'), path.name)
         for path in brewpath.iterdir()
-        if (brew_group := re.match(r"(?P<module>[a-zA-Z]+)\.Brewfile$", path.name))
+        if (
+            brew_group := re.match(
+                r'(?P<module>[a-zA-Z]+)\.Brewfile$', path.name
+            )
+        )
     ],
     key=lambda item: item[0],
 )
@@ -71,98 +75,118 @@ brew_groups, brewfiles = zip(*brew_specs)
 
 terminal_menu = TerminalMenu(
     brew_groups,
-    title="Which collections should brew install?",
+    title='Which collections should brew install?',
     multi_select=True,
     show_multi_select_hint=True,
-    preselected_entries=["cli", "common", "emacs", "nushell"],
+    preselected_entries=['cli', 'common', 'emacs', 'nushell'],
 )
 selected_indices = terminal_menu.show()
 
 selected_brewfiles = {brew_groups[i]: brewfiles[i] for i in selected_indices}
 
-LOG.info("This could take a while... plug me in and go touch some grass.")
+LOG.info('This could take a while... plug me in and go touch some grass.')
 for groupname, brewfile in selected_brewfiles.items():
     brewfile_path = brewpath / brewfile
-    LOG.info("Installing %s dependencies...", groupname)
-    subprocess.run(["brew", "bundle", "install", "--file", brewfile_path])
-    LOG.info("Finished installing %s dependencies!", groupname)
+    LOG.info('Installing %s dependencies...', groupname)
+    subprocess.run(['brew', 'bundle', 'install', '--file', brewfile_path])
+    LOG.info('Finished installing %s dependencies!', groupname)
 
-if "latex.Brewfile" in selected_brewfiles:
-    LOG.info("LaTeX option selected; installing digestif.")
-    subprocess.run(["sh", str(pathlib.Path(__file__).parent / "install-digestif.sh")])
+if 'latex.Brewfile' in selected_brewfiles:
+    LOG.info('LaTeX option selected; installing digestif.')
+    subprocess.run(
+        ['sh', str(pathlib.Path(__file__).parent / 'install-digestif.sh')]
+    )
 
-LOG.info("Linking emacs")
+LOG.info('Linking emacs')
 # TODO this part might not be necessary, need to test
-subprocess.run(["brew", "link", "emacs-mac"])
+subprocess.run(['brew', 'link', 'emacs-mac'])
 # see `brew info emacs-mac`
 subprocess.run(
     [
-        "osascript",
-        "-e",
+        'osascript',
+        '-e',
         """'tell application "Finder" to make alias file to POSIX file "/opt/homebrew/opt/emacs-mac/Emacs.app" at POSIX file "/Applications"'""",
     ]
 )
 
-LOG.info("Installing chemacs")
+LOG.info('Installing chemacs')
 subprocess.run(
     [
-        "git",
-        "clone",
-        "https://github.com/plexus/chemacs2.git",
-        str(pathlib.Path.home() / ".emacs.d"),
+        'git',
+        'clone',
+        'https://github.com/plexus/chemacs2.git',
+        str(pathlib.Path.home() / '.emacs.d'),
     ]
 )
 
-LOG.info("Installing doom")
+LOG.info('Installing doom')
 subprocess.run(
     [
-        "git",
-        "clone",
-        "https://github.com/hlissner/doom-emacs",
-        str(pathlib.Path.home() / ".config/doom-emacs"),
+        'git',
+        'clone',
+        'https://github.com/hlissner/doom-emacs',
+        str(pathlib.Path.home() / '.config/doom-emacs'),
     ]
 )
-subprocess.run(["doom", "install", "-!"])
+subprocess.run(['doom', 'install', '-!'])
+
+LOG.info('Linking doom (so topgrade can find it)')
+subprocess.run(
+    [
+        'mkdir',
+        '~/.emacs.d/bin',
+    ]
+)
+subprocess.run(
+    [
+        'ln',
+        '-svf',
+        '~/.config/doom-emacs/bin/doom',
+        '~/.emacs.d/bin/doom',
+    ]
+)
 
 LOG.info(
     "Adding `dots` alias for `...` dir so nushell can find it (without 'smart'ly resolving the ...)"
 )
 subprocess.run(
     [
-        "ln",
-        "-svf",
+        'ln',
+        '-svf',
         str(DOTFILE_PATH),
-        str(pathlib.Path.home() / ".config" / "dots"),
+        str(pathlib.Path.home() / '.config' / 'dots'),
     ]
 )
 
 # set up from other scripts
 # TODO maybe move these eventually
 # anki-connect (so emacs can talk to anki)
-LOG.info("Setting up anki connect.")
-subprocess.run(["sh", str(pathlib.Path(__file__).parent / "anki-connect-mac-setup.sh")])
+LOG.info('Setting up anki connect.')
+subprocess.run(
+    ['sh', str(pathlib.Path(__file__).parent / 'anki-connect-mac-setup.sh')]
+)
 # osx settings
-LOG.info("Setting MacOS settings.")
-subprocess.run(["sh", str(pathlib.Path(__file__).parent / "osxdefaults.sh")])
+LOG.info('Setting MacOS settings.')
+subprocess.run(['sh', str(pathlib.Path(__file__).parent / 'osxdefaults.sh')])
 
 # link nushell, since it doesn't support XDG-based config for some reason
 # also set up zoxide
-LOG.info("Linking nushell (for MacOS)")
+LOG.info('Linking nushell (for MacOS)')
 subprocess.run(
     [
-        "ln",
-        "-svf",
-        str(pathlib.Path.home() / ".config/nu"),
-        str(pathlib.Path.home() / "Library/Application Support/nushell"),
+        'ln',
+        '-svf',
+        str(pathlib.Path.home() / '.config/nu'),
+        str(pathlib.Path.home() / 'Library/Application Support/nushell'),
     ]
 )
-with open(str(pathlib.Path.home() / ".local/share/.zoxide.nu"), "w") as f:
-    subprocess.call(["zoxide", "init", "nushell"], stdout=f)
+with open(str(pathlib.Path.home() / '.local/share/.zoxide.nu'), 'w') as f:
+    subprocess.call(['zoxide', 'init', 'nushell'], stdout=f)
 
-LOG.info("Starting brew services")
+LOG.info('Starting brew services')
 BREW_SERVICES = [
-    "yabai",
-    "skhd",
+    'yabai',
+    'skhd',
 ]
 for service in BREW_SERVICES:
-    subprocess.run(["brew", "services", "start", service])
+    subprocess.run(['brew', 'services', 'start', service])
