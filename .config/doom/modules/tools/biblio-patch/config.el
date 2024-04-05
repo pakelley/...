57a37a0230a38068edcb3484d432d1b97528eeb8
pakelley <patrick@heartex.com>
2024-04-05 10:16:43 -0700

    [doom/biblio] citar enhancements

--- .config/doom/modules/tools/biblio-patch/config.el
+++ .config/doom/modules/tools/biblio-patch/config.el
@@ -4,3 +4,11 @@
   (setq citar-bibliography org-cite-global-bibliography))
 
 (after! oc (setq org-cite-csl-styles-dir "~/Zotero/styles"))
+
+(use-package! citar-org-roam
+  :after (citar org-roam)
+  :custom
+  (citar-org-roam-subdir "literature-notes")
+  ;; Tell citar-org-roam which capture template to use (as suggested in the README)
+  (citar-org-roam-capture-template-key "n")
+  )
